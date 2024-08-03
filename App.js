06f415e9cd106997f62bfdfd89497dd5c2ba4fbc
Dilshan Warnasuriya <dilshan.se.dew@gmail.com>
2024-08-04 02:37:12 +0530

    re design interface

--- App.js
+++ App.js
@@ -2,20 +2,48 @@
 let locationtxt = document.getElementById("txt-location");
 let temperature = document.getElementById("temperature");
 let Weather = document.getElementById("WeatherName");
-let image = document.querySelector('.result-image img');
+let nowLocation = document.getElementById("location");
+let wind = document.getElementById("wind");
+let image = document.querySelector('.image img');
 
 function find() {
 
-    fetch("https://api.weatherapi.com/v1/current.json?key=e5d078b798b44537bd7155559241404&q=" + locationtxt.value)
-        .then(res => res.json())
-        .then(data => {
-            temperature.innerHTML = `<b>${data.current.temp_c}&deg;C</b>`;
-            Weather.innerHTML = data.current.condition.text;
 
-            let icon = data.current.condition.text
-            image.src = 'image/' + icon + '.png';
-
-            results.style = "visibility: visible; transition: transform 3s ease;";
-        })
 }
 
+$(document).ready(function () {
+    $('.search button').click(function () {
+        if (locationtxt.value == "") {
+            $('.result').slideUp();
+        }
+        else {
+
+            fetch("https://api.weatherapi.com/v1/current.json?key=e5d078b798b44537bd7155559241404&q=" + locationtxt.value)
+                .then(res => res.json())
+                .then(data => {
+
+                    if(!data.location){
+                        $('.result').slideUp();
+                    }
+                    else{
+
+                        let roundedNumber = Math.round(data.current.temp_c);
+                        
+                        temperature.innerHTML = `<b>${roundedNumber}&deg;</b>`;
+                        Weather.innerHTML = data.current.condition.text;
+                        nowLocation.innerHTML = data.location.name + ", " + data.location.country;
+                        wind.innerHTML = "Wind : " + data.current.wind_kph + " kph"
+        
+                        let icon = data.current.condition.text
+                        image.src = 'image/' + icon + '.png';
+
+
+                        $('.result').slideDown();
+                    }
+
+                }
+            )
+        }
+
+    });
+});
